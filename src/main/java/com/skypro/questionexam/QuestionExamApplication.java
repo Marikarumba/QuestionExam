e8dc20143f512e24b8351d64bc187ad72ad3c81b
Marika <marikarumba@mail.ru>
2022-12-24 23:14:50 +0300

    Initial commit

---
+++ src/main/java/com/skypro/questionexam/QuestionExamApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.questionexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/questionexam/QuestionExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.questionexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
